955fbf6aaba5e798abaa9579830e7aa3ce8d39ea
dselva-2005 <dselvajagan@gmail.com>
2024-12-22 13:52:15 +0530

    test

--- static/js/main.js
+++ static/js/main.js
@@ -214,3 +214,13 @@
     });
 
 })(jQuery);
+
+document.addEventListener('DOMContentLoaded',()=>{
+    //cancelling dismissable notification/ message
+    alerts = Array.from(document.getElementsByClassName('killAlert'))
+    alerts.forEach(element => {
+        element.onclick = (event) => {
+        event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+       }
+    });
+})
