ec69f13703a7728b5a2ee6429ead2b766e67b424
dselva-2005 <dselvajagan@gmail.com>
2024-12-22 13:36:01 +0530

    added notification?

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/',views.login_view,name='login'),
     path('',include('django.contrib.auth.urls')),
     path('register',views.register_view,name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
 from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def register_view(request):
     if request.method == 'POST':
@@ -15,4 +17,19 @@ def register_view(request):
         form = CustomUserCreationForm()
 
     return render(request, 'registration/register.html', {'form': form})
- 
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Log in the user
+            login(request, user)
+            messages.success(request, 'login was successful!')
+            return redirect('shop:home')  # Redirect to a success page
+        else:
+            # Invalid login credentials
+            messages.error(request,'Invalid Credentials')
+            return render(request, 'login.html')
+    return render(request, 'registration/login.html')
